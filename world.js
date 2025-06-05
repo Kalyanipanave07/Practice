29600ea824c84bafce5e35888174770684d7edf3
Kalyanipanave07 <panvekalyani447@gmail.com>
2025-06-05 13:22:56 +0530

    learning

---
+++ world.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+let val=19
+val='this'
+console.log(val)
+const bal=3.14
+
+console.log(bal)
+let i=11;
+if(i%2==0){
+    console.log ( i +"Even Number");
+}
+else{
+    console.log ( i+  "Odd number");
+}
+
+
